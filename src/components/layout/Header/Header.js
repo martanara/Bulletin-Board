44d8c2823e0455c6b087743da10139c24e37d0f7
Marta Nara <marta.kos1230@gmail.com>
2022-04-30 13:51:48 +0200

    Add styling and google auth button to Header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Header = () => {
   return (
-    <AppBar position="relative">
+    <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" color="inherit" noWrap>
-          Bulletin Board
-        </Typography>
+        <Grid container  direction="row" justifyContent="space-between" alignItems="center">
+          <Grid item xs={7} md={9} lg={10}>
+            <Typography variant="h6" color="inherit">
+                Bulletin Board
+            </Typography>
+          </Grid>
+          <Grid item xs={5} md={3} lg={2}>
+            <Button href="https://www.google.pl/" color="inherit" variant="outlined">Login with Google</Button>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
